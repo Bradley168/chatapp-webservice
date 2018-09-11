c12d4549fbcee129990c767247b75d914cb88f56
Bradley <bradley.heng@pathmazing.com>
2018-09-11 09:45:29 +0700

    remove unused module

--- src/api/shared/shared.module.ts
+++ src/api/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 
-import { FirebaseAdminService } from './firebase-admin.service';
 import { MailerService } from './mailer.service';
-import { S3Service } from './s3.service';
 
 @Global()
 @Module({
-  providers: [FirebaseAdminService, MailerService, S3Service],
-  exports: [FirebaseAdminService, MailerService, S3Service]
+  providers: [MailerService],
+  exports: [MailerService]
 })
 export class SharedModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,20 @@ import { Module } from '@nestjs/common';
 
 import { ApiModule } from './api/api.module';
 import { ConfigModule } from './lib/config/config.module';
-import { FirebaseAdminModule } from './lib/firebase-admin';
 import { MailerModule } from './lib/mailer';
 import { SequelizeModule } from './lib/sequelize';
-import { SocialModule } from './lib/social';
-import { SocketModule } from './lib/socket/socket.module';
 
 @Module({
   imports: [
     ApiModule,
     ConfigModule,
-    FirebaseAdminModule,
+    // FirebaseAdminModule,
     // GoogleCloudStorageModule,
     MailerModule,
     // MongooseModule,
     // RedisModule,
-    SocialModule,
-    SocketModule,
+    // SocialModule,
+    // SocketModule,
     // S3Module,
     SequelizeModule
   ]
